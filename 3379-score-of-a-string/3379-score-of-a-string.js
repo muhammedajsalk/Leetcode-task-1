ca9dd5e292f987cead9abb6629b2c533c14cafa8
muhammed ajsal <133773108+muhammedajsalk@users.noreply.github.com>
2025-10-11 12:26:09 +0530

    Time: 0 ms (100%), Space: 55.7 MB (36.08%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.js
+++ 3379-score-of-a-string/3379-score-of-a-string.js
@@ -3,28 +3,9 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let a=s.split("")
-    let b=a.map((item,index)=>{
-        return item.charCodeAt(0)
-    })
-    let c=b.map((item,index)=>{
-        return b[index]-b[index+1]
-    })
-    
-    let d=c.map((item,index)=>{
-        if(item<=0){
-            return Math.abs(item)
-        }
-        return item
-    })
-    
-    let e=d.filter((item,index)=>{
-        return item === Number(item)
-    })
-    
-    let f=e.reduce((acc,item)=>{
-        return acc+item
-    },0)
-    
-    return f
+     let sum = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+    }
+    return sum;
 };
